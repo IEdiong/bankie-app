f240e9dd0c40699fc543ed09c36db1b40828500f
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-10-23 11:59:56 +0100

    add routing to dashboard

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [SidebarComponent, HeaderComponent],
 })
 export class SharedModule {}
